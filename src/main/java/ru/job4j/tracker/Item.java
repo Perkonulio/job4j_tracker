9311763127b7b196c0af4024f0a158ddb0652d65
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2020-12-17 13:56:13 +0300

    7. Date. Отображение даты. [#333250]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -8,14 +8,20 @@ public class Item {
     private String name;
     private LocalDateTime created = LocalDateTime.now();
 
-    public LocalDateTime getCreated() {
-        return created;
+    public Item() {
+
     }
 
-    public void StartUI() {
-        Item item = new Item();
-        System.out.println("Текущие дата и время : " +
-                item.getCreated().format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")));
+    public Item(int id) {
+
+    }
+
+    public Item(int id, String name) {
+
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public int getId() {
