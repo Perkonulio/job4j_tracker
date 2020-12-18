4c9e38906baab8ac264b398cd60188a18b74b146
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2020-12-18 16:20:46 +0300

    2. Модель данных. [#333277]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,13 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("John");
+        student.setSurname("Brahlovski");
+        student.setNumberGroup(15);
+        student.setDate("15.09.2010");
+        System.out.println(student.getName() + " " + student.getSurname() + " " + student.getNumberGroup() +
+                " " + student.getDate());
+    }
+}
