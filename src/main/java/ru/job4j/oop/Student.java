730396e26a5f551ca6c972900c1aa8b1812d8654
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2021-01-21 11:19:48 +0300

    3.1. Приведение типов. Повышение типа и понижение типа. [#333210]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -2,21 +2,6 @@ package ru.job4j.oop;
 
 import java.rmi.StubNotFoundException;
 
-public class Student {
-    public void song() {
-        System.out.println("I believe I can fly");
-    }
-    public void music() {
-        System.out.println("Tra tra tra");
-    }
+public class Student extends Object{
 
-    public static void main(String[] args) {
-        Student petya = new Student();
-        petya.music();
-        petya.music();
-        petya.music();
-        petya.song();
-        petya.song();
-        petya.song();
-    }
 }
