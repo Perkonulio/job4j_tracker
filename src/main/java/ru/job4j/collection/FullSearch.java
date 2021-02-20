4ae3de442137e33fc3e1e5c236817bd0ddc4bea3
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2021-02-20 07:34:39 +0300

    1. Уникальные задачи. [#333197]

---
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+
+public class FullSearch {
+    public static HashSet<String> extractNumber(List<Task> task) {
+        HashSet<String> numbers = new HashSet<>();
+        for(Task list : task) {
+            numbers.add(list.getNumber());
+        }
+        return numbers;
+
+    }
+
+}
---
+++ src/test/java/ru/job4j/collection/FullSearchTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class FullSearchTest {
+    @Test
+    public void exctractNumberTest() {
+        List<Task> list = Arrays.asList(new Task("1", "Desc1"),
+                new Task("2", "Desc2"), new Task("1", "Desc1"));
+        Set<String> expected = new HashSet<>(Arrays.asList("1", "2"));
+        assertThat(FullSearch.extractNumber(list), is(expected));
+
+    }
+}
