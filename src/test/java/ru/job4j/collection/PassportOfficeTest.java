e55a7bcc07ec0a1108c80ae5a5415c5e0cd48e35
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2021-02-26 13:22:30 +0300

    1. Паспорт и жители [#333193]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void add() {
+        PassportOffice pass = new PassportOffice();
+        Citizen citizen = new Citizen("9504", "Petr");
+        pass.add(citizen);
+        assertThat(pass.getCitizens(citizen.getPassport()), is(citizen));
+    }
+
+
+
+}
