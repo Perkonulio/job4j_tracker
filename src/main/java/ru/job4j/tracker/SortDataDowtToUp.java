47ef8cc326ef9ffab22fc17537b005d4beb80418
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2021-03-03 14:42:32 +0300

    0. Сортировка [#333182]

---
+++ src/main/java/ru/job4j/tracker/SortDataDowtToUp.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker;
+
+import java.util.Comparator;
+
+public class SortDataDowtToUp implements Comparator<Item> {
+
+    @Override
+    public int compare(Item o1, Item o2) {
+        return o1.getId() - o2.getId();
+    }
+}
