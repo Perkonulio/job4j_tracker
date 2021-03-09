196a0f9291a340baf70fe03ae05ed7ae39a8c3e3
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2021-03-09 08:22:37 +0300

    0. Сортировка [#333182]

--- src/main/java/ru/job4j/tracker/SortDataUpToDown.java
+++ src/main/java/ru/job4j/tracker/SortDataUpToDown.java
@@ -6,6 +6,6 @@ public class SortDataUpToDown implements Comparator<Item> {
 
     @Override
     public int compare(Item o1, Item o2) {
-        return o2.getId() - o1.getId();
+        return Integer.compare(o2.getId(), o1.getId());
     }
 }
--- src/test/java/ru/job4j/tracker/SortDataUpToDownTest.java
+++ src/test/java/ru/job4j/tracker/SortDataUpToDownTest.java
@@ -1,5 +1,28 @@
 package ru.job4j.tracker;
 
+import org.junit.Test;
+import ru.job4j.collection.Job;
+
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+
 public class SortDataUpToDownTest {
+    @Test
+    public void whenDataUp() {
+        Item item1 = new Item("Pedro");
+        Item item2 = new Item("Hulio");
+        SortDataUpToDown sorting = new SortDataUpToDown();
+        List<Item> list = Arrays.asList(item1, item2);
+        list.sort(sorting);
+        List<Item> listExpected = Arrays.asList(item1, item2);
+        assertThat(list, is(listExpected));
+    }
+
+
+
+
 
 }
