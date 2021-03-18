b7de0ebb29bebc2914c0fe672e81e89bce122c31
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2021-03-18 16:16:35 +0300

    Отсортировать департаменты [#333174]

---
+++ src/test/java/ru/job4j/collection_final/DepartamentsTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.collection_final;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class DepartamentsTest {
+
+    @Test
+    public void whenMissed() {
+        List<String> input = Arrays.asList("k1", "k1/ssk2");
+        List<String> expect = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1", "k1/sk1/ssk2");
+        List<String> result = Departaments.fillGaps(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNonChange() {
+        List<String> input = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> result = Departaments.fillGaps(input);
+        assertThat(result, is(expect));
+    }
+}
