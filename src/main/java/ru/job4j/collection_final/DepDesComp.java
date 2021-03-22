8d527532ebef098a7b826e18ea22ec8bf4e11480
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2021-03-22 14:41:13 +0300

    Отсортировать департаменты [#333174]

--- src/main/java/ru/job4j/collection_final/DepDesComp.java
+++ src/main/java/ru/job4j/collection_final/DepDesComp.java
@@ -8,11 +8,7 @@ public class DepDesComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String o1Result = o1.split("/")[0];
         String o2Result = o2.split("/")[0];
-        int result;
-        result = o2Result.compareTo(o1Result);
-        if (result == 0) {
-            return o1.compareTo(o2);
-        }
-        return result;
+        int result = o2Result.compareTo(o1Result);
+        return (result == 0) ? o1.compareTo(o2) : result;
     }
 }
