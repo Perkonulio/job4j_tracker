fc44e375b2241ac518536442bc5606b9fdad12ed
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2021-03-24 15:23:13 +0300

    3. Ссылки на методы [#333020]

---
+++ src/main/java/ru/job4j/lambda/RefMethod.java
@@ -0,0 +1,22 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class RefMethod {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Ivan", "Petr Arsentev");
+        Consumer<String> out = RefMethod::cutOut;
+        names.forEach(out);
+    }
+
+    public static void cutOut(String name) {
+        if(name.length() > 10) {
+            System.out.println(name.substring(0, 10) + "...");
+        } else {
+            System.out.println(name);
+        }
+    }
+}
