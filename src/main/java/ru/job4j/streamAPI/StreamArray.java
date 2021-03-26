6b9c4d27ec9d08447c5926c1bc7010aa2ded5c6a
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2021-03-26 17:21:20 +0300

    0. Stream API [#333031]

---
+++ src/main/java/ru/job4j/streamAPI/StreamArray.java
@@ -0,0 +1,15 @@
+package ru.job4j.streamAPI;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamArray {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, -2, 3, 4);
+        List<Integer> listOut = list.stream().filter(elementList -> elementList >= 0)
+                .collect(Collectors.toList());
+        listOut.forEach(System.out::println);
+    }
+}
