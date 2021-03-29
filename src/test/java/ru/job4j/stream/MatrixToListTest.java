8740a808bf34e8ee9ab0e5809288b8c0c16b0ee1
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2021-03-29 12:42:33 +0300

    5. Преобразование матрицы чисел в список чисел. [#333036]

---
+++ src/test/java/ru/job4j/stream/MatrixToListTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.stream;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class MatrixToListTest {
+
+    @Test
+    public void whenExpectedTrue() {
+        Integer[][] mass = new Integer[][] {
+                {1, 2},
+                {3, 4}
+        };
+        List<Integer> expected = List.of(1, 2, 3, 4);
+        MatrixToList mat = new MatrixToList();
+        List<Integer> listForExpect = mat.matrixToList(mass);
+        assertThat(listForExpect, is(expected));
+
+
+    }
+}
