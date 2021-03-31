3effeab4cf6114d842e40790cb390b52edf17af1
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2021-03-31 14:07:52 +0300

    4. Optional в Stream API. [#333046]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -12,6 +12,7 @@ public class BankService {
     /**
      * Метод принимает на вход пользователя
      * и добавляет его в HashMap, если его там нет.
+     *
      * @param user пользователь который добавляется в HashMap.
      */
     public void addUser(User user) {
@@ -22,13 +23,14 @@ public class BankService {
      * Метод принимает на вход номер паспорта от User,
      * и аккаунт который нужно добавить
      * в список аккаунтов привязанных к User, если его там нет.
+     *
      * @param passport номер паспорта по которому идет поиск пользователя
-     * @param account аккаунт который необходимо добавить
+     * @param account  аккаунт который необходимо добавить
      */
     public void addAccount(String passport, Account account) {
-        User user = findByPassport(passport);
-        if (user != null) {
-            List<Account> list = users.get(user);
+        Optional<User> user = findByPassport(passport);
+        if (user.isPresent()) {
+            List<Account> list = users.get(user.get());
             if (!list.contains(account)) {
                 list.add(account);
             }
@@ -37,52 +39,54 @@ public class BankService {
 
     /**
      * Метод который осуществляет поиск ползователя по номеру паспорта.
+     *
      * @param passport номер паспорта для осуществления поиска по HashMap
      * @return возвращает пользователя из HashMap, если он там есть
      */
-    public User findByPassport(String passport) {
+    public Optional<User> findByPassport(String passport) {
         return users.keySet()
                 .stream()
                 .filter(user -> user.getPassport().equals(passport))
-                .findFirst().orElse(null);
+                .findFirst();
     }
 
     /**
      * Метод осуществляет поиск аккаунта
      * по реквизитам и пользователю привязанных к аккаунту.
-     * @param passport номер паспорта для осуществления поиска по HashMap
+     *
+     * @param passport  номер паспорта для осуществления поиска по HashMap
      * @param requisite реквизиты связанные с аккаунтом, для поиска последнего
      * @return возвращает аккаунт из HashMap, если он там есть
      */
-    public Account findByRequisite(String passport, String requisite) {
-        User user = findByPassport(passport);
-        if (user != null) {
-            return users.get(user)
+    public Optional<Account> findByRequisite(String passport, String requisite) {
+        Optional<User> user = findByPassport(passport);
+        if (user.isPresent()) {
+            return users.get(user.get())
                     .stream()
                     .filter(account -> account.getRequisite().equals(requisite))
-                    .findFirst()
-                    .orElse(null);
+                    .findFirst();
         }
-        return null;
+        return Optional.empty();
     }
 
     /**
      * Метод осуществляет перевод средств по реквизитам и номеру пасспорта,
      * с одного аккаунта на другой.
-     * @param srcPassport номер паспорта по которому осуществляется поиск аккаунта
-     * @param srcRequisite реквизиты привязанные к аккаунту С КОТОРЫХ необходимо перевести средства
-     * @param destPassport номер паспорта по которму осуещствляется поиск второго аккаунта
+     *
+     * @param srcPassport   номер паспорта по которому осуществляется поиск аккаунта
+     * @param srcRequisite  реквизиты привязанные к аккаунту С КОТОРЫХ необходимо перевести средства
+     * @param destPassport  номер паспорта по которму осуещствляется поиск второго аккаунта
      * @param destRequisite реквизиты с второго аккаунта НА КОТОРЫЙ необходимо перевести средства
-     * @param amount указываемая сумма средств для перевода
+     * @param amount        указываемая сумма средств для перевода
      * @return возвращает результат перевода средств
      */
     public boolean transferMoney(String srcPassport, String srcRequisite, String destPassport,
-                                  String destRequisite, double amount) {
-        Account account1 = findByRequisite(srcPassport, srcRequisite);
-        Account account2 = findByRequisite(destPassport, destRequisite);
-        if (account1 != null && account2 != null && account1.getBalance() >= amount) {
-            account2.setBalance(amount + account2.getBalance());
-            account1.setBalance(account1.getBalance() - amount);
+                                 String destRequisite, double amount) {
+        Optional<Account> account1 = findByRequisite(srcPassport, srcRequisite);
+        Optional<Account> account2 = findByRequisite(destPassport, destRequisite);
+        if (account1.isPresent() && account2.isPresent() && account1.get().getBalance() >= amount) {
+            account2.get().setBalance(amount + account2.get().getBalance());
+            account1.get().setBalance(account1.get().getBalance() - amount);
         }
         return false;
     }
